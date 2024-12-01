9d8e281f02dbd86e355b2599b2239992853a723d
konradRduch <91778942+konradRduch@users.noreply.github.com>
2024-12-01 08:54:50 +0100

    changes 3

--- src/app/modules/core/models/activity.model.ts
+++ src/app/modules/core/models/activity.model.ts
@@ -3,13 +3,17 @@ export class Activity {
     public title: string;
     public category: string;
     public description: string;
+    public startDate: string;
+    public endDate: string;
     public imageUrl: string;
     
-    constructor(id: string, title: string,category: string, description: string, imageUrl: string ){
+    constructor(id: string, title: string,category: string, description: string,startDate: string,endDate: string, imageUrl: string ){
         this.id = id ;
         this.title = title;
         this.category = category;
         this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.imageUrl = imageUrl;
     }
 }
